bbd38e180217a79faae21aecf017e3bddd7dae5e
Prescott Rowe <prescottrowe@gmail.com>
2020-05-04 23:06:16 -0700

    basic tick tack toe

---
+++ TicyTackyToe/TicyTackyToe/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicyTackyToe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TicyTackyToe.MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
